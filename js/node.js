2d1fe63f517c370160b52ef9e82d0f4fdd91c4d5
Drew Gunderson <drewgun@gmail.com>
2016-10-03 17:55:56 -0400

    Added function to calculate the distance between two nodes in two dimensions.
    Added test functions to test node construction and distance calculation.

--- js/node.js
+++ js/node.js
@@ -5,6 +5,14 @@ try {
     beyondTheFold.node = {};
 }
 
-class Node {
+function calculateDistance(nodeOne, nodeTwo) {
+	var dx = nodeOne.coordinates[0] - nodeTwo.coordinates[0];
+	var dy = nodeOne.coordinates[1] - nodeTwo.coordinates[1];
+	return(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
+}
 
+class Node {
+	constructor(x, y) {
+		this.coordinates = [x, y];
+	}
 }
